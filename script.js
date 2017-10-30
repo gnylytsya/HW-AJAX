3045682cb03f156e8d6d4f369b587dc180ff7193
gnylytsya <gnylytsya@gmail.com>
2017-10-31 01:27:08 +0200

    add script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+var xhr = new XMLHttpRequest();
+xhr.open("get", )
+
+$.getJSON([api_url], function(forecast) {
+	console.log(forecast);
+});
+
+AIzaSyBZ-_EI53ixIKRgy4Mh2h-aQmFAawTwVPU
+https://api.darksky.net/forecast/d0c6a67fc20160d90304109a4bf217fc/37.8267,-122.4233
+d0c6a67fc20160d90304109a4bf217fc
+$.getJSON('https://api.darksky.net/forecast/[d0c6a67fc20160d90304109a4bf217fc]/[latitude],[longitude]', function(forecast) {});
+
+<script src="https://maps.googleapis.com/maps/api/js?key=[AIzaSyBZ-_EI53ixIKRgy4Mh2h-aQmFAawTwVPU]&libraries=places,geometry"></script>
