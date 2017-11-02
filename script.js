4ab374cd820d6c5553a5ccee56df2d14b5705a34
gnylytsya <gnylytsya@gmail.com>
2017-11-02 17:52:04 +0200

    add script.js

--- script.js
+++ script.js
@@ -1,30 +1,37 @@
-var xhr = new XMLHttpRequest();
-xhr.open("get", )
+//var xhr = new XMLHttpRequest();
+//xhr.open("get", )
+//
+//$.getJSON([api_url], function(forecast) {
+//	console.log(forecast);
+//});
+//
+//AIzaSyBZ-_EI53ixIKRgy4Mh2h-aQmFAawTwVPU
+//https://api.darksky.net/forecast/d0c6a67fc20160d90304109a4bf217fc/37.8267,-122.4233
+//d0c6a67fc20160d90304109a4bf217fc
+//
+//<script src="https://maps.googleapis.com/maps/api/js?key=[AIzaSyBZ-_EI53ixIKRgy4Mh2h-aQmFAawTwVPU]&libraries=places,geometry"></script>
+//
+//
+//$.getJSON('https://api.darksky.net/forecast/[d0c6a67fc20160d90304109a4bf217fc]/[latitude],[longitude]', function(forecast) {
+//	
+//	
+//});
 
-$.getJSON([api_url], function(forecast) {
+$.getJSON([https: api.darksky.net/forecast/[d0c6a67fc20160d90304109a4bf217fc], function(forecast) {
 	console.log(forecast);
 });
 
-AIzaSyBZ-_EI53ixIKRgy4Mh2h-aQmFAawTwVPU
-https://api.darksky.net/forecast/d0c6a67fc20160d90304109a4bf217fc/37.8267,-122.4233
-d0c6a67fc20160d90304109a4bf217fc
-$.getJSON('https://api.darksky.net/forecast/[d0c6a67fc20160d90304109a4bf217fc]/[latitude],[longitude]', function(forecast) {});
-
-<script src="https://maps.googleapis.com/maps/api/js?key=[AIzaSyBZ-_EI53ixIKRgy4Mh2h-aQmFAawTwVPU]&libraries=places,geometry"></script>
-
-
-
-
-var weather = new XMLHttpRequest();
-weather.open("GET", "https://api.darksky.net/forecast/[d0c6a67fc20160d90304109a4bf217fc]/[latitude],[longitude]");
-weather.send(null);
-
-var r = JSON.parse(weather.response);
-var dis = "Current location"
-
-
-https://api.darksky.net/forecast/[d0c6a67fc20160d90304109a4bf217fc]/[latitude],[longitude]
-
+//
+//var weather = new XMLHttpRequest();
+//weather.open("GET", "https://api.darksky.net/forecast/[d0c6a67fc20160d90304109a4bf217fc]/[latitude],[longitude]");
+//weather.send(null);
+//
+//var r = JSON.parse(weather.response);
+//var dis = "Current location"
+//
+//
+//https://api.darksky.net/forecast/[d0c6a67fc20160d90304109a4bf217fc]/[latitude],[longitude]
+//
 
 
 
